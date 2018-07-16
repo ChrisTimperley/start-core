feffa3e4d2b658f2608b9a49e8c09d96d57e8301
ChrisTimperley <christimperley@googlemail.com>
2018-07-16 18:50:54 -0400

    added basic setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+import glob
+import setuptools
+
+PKG_NAME = 'start_th'
+
+path = os.path.join(os.path.dirname(__file__), 'src', PKG_NAME, 'version.py')
+with open(path, 'r') as f:
+    exec(f.read())
+
+setuptools.setup(
+    name=PKG_NAME,
+    version=__version__,
+    description='A test harness for START',
+    long_description='TBA',
+    author='Chris Timperley',
+    author_email='christimperley@gmail.com',
+    url='https://github.com/ChrisTimperley/start-test-harness',
+    # python_requires='>=3.5',
+    install_requires=[],
+    packages=['start_th'],
+    package_dir={'': 'python'},
+    entry_points = {
+        'console_scripts': [ 'start-tester = start_th.cli:main' ]
+    }
+)
