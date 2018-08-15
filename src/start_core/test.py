34066cca074c40a49a62828d54146187bc96ab6d
ChrisTimperley <christimperley@googlemail.com>
2018-08-15 14:35:31 -0400

    refactored test harness

--- src/start_core/test.py
+++ src/start_core/test.py
@@ -1,137 +1,99 @@
-#!/usr/bin/env python
-#
-# This file is responsible for implementing a GenProg-style test harness for
-# a given scenario.
-#
+"""
+This module is responsible for implementing a GenProg-style test harness for
+a given scenario.
+"""
+__all__ = ['execute']
+
 from __future__ import print_function
-import math
-import sys
-import os
-import configparser
-import pprint
-import argparse
+from typing import Tuple
+import logging
 
 import dronekit
 
-import sitl
-import mission
-import attack
-import helper
+from .sitl import SITL
+from .mission import Mission
+from .attack import Attack, Attacker
+from .exceptions import TimeoutException
+
+logger = logging.getLogger(__name__)  # type: logging.Logger
+logger.setLevel(logging.DEBUG)
 
 
-class TestCase(object):
-    def __init__(self,
-                 fn_mission, # type: str
-                 use_attacker = False, # type: Optional[bool]
-                 fn_cfg_scenario = None, # type: Optional[str]
-                 fn_cfg_default = None, # type: Optional[str]
-                 dir_source = '/opt/ardupilot', # type: Optional[str],
-                 connection_timeout = 10, # type: Optional[int],
-                 home = None # type: Optional[Tuple[float, float, float, float]]
-                 ): # type: None
-        """
-        Constructs a new test case.
+# type: (str, bool)
+def execute(mission,                # type: Mission
+            attack=None,            # type: Optional[Attack]
+            speedup=1,              # type: int
+            prefix='',
+            timeout_mission=240,    # type: int
+            timeout_liveness=1,     # type: int
+            timeout_connection=10,  # type: int
+            check_wps=False,        # type: bool
+            enable_workaround=True  # type: bool
+            ):              # type: (...) -> Tuple[bool, str]
+    """
+    Executes the test.
 
-        Parameters:
-            end_pos: the expected position of the vehicle following the
-                completion of the test.
-            use_attacker: a flag indicating whether this test case should
-                perform the attack.
+    Parameters:
+        sitl_prefix: a command to prefix to the SITL binary. (used to
+            attach valgrind, for example).
+        speedup: the speedup factor that should be used by the simulator.
 
-        TODO:
-            accept a scenario file
-        """
-        if not fn_cfg_scenario:
-            fn_cfg_scenario = "/experiment/config/scenario.cfg"
-        if not fn_cfg_default:
-            fn_cfg_default = "/experiment/config/default.cfg"
+    Returns:
+        a tuple of the form `(passed, reason)`, where `passed` is a flag
+        that indicates whether or not the test succeeded, and `reason` is
+        an optional string that is used to describe the reason for the
+        test failure (if indeed there was a failure).
+    """
+    trace = []  # FIXME
+    vehicle = None
+    url_sitl = 'udp:127.0.0.1:14550'
+    sitl = SITL.from_scenario(scenario)  # FIXME
+    if attack:
+        attacker = Attacker(attack, url_sitl, port)  # FIXME
+    else:
+        attacker = None
 
-        assert os.path.isfile(fn_cfg_scenario), \
-            "could not find scenario config: {}".format(fn_cfg_scenario)
-        assert os.path.isfile(fn_cfg_default), \
-            "could not find default config: {}".format(fn_cfg_default)
+    try:
+        with sitl.launch(prefix, speedup):
+            if attacker:
+                attacker.prepare()
 
-        self.__connection_timeout = connection_timeout
-
-        # load the config file for this scenario
-        self.__cfg = configparser.SafeConfigParser()
-        self.__cfg.read(fn_cfg_default)
-        self.__cfg.read(fn_cfg_scenario)
-
-        self.__sitl = sitl.SITL.from_cfg(self.__cfg,
-                                         dir_source=dir_source,
-                                         home=home)
-        self.__mission = \
-            mission.Mission.from_file(home_location=self.__sitl.home,
-                                      vehicle_kind=self.__sitl.vehicle,
-                                      fn=fn_mission)
-
-        if use_attacker:
-            self.__attacker = attack.Attacker.from_cfg(self.__cfg)
-        else:
-            self.__attacker = None
-
-    def execute(self,
-                sitl_prefix = None,
-                speedup = 1,
-                time_limit = 240,
-                liveness_timeout = 1,
-                check_wps = False,
-                enable_workaround = True
-                ):
-        """
-        Executes the test.
-
-        Parameters:
-            sitl_prefix: a command to prefix to the SITL binary. (used to
-                attach valgrind, for example).
-            speedup: the speedup factor that should be used by the simulator.
-
-        Returns:
-            a tuple of the form `(passed, reason)`, where `passed` is a flag
-            that indicates whether or not the test succeeded, and `reason` is
-            an optional string that is used to describe the reason for the
-            test failure (if indeed there was a failure).
-        """
-        # sitl, vehicle, attacker
-        trace = []
-        vehicle = None
-        try:
-            # let's prep the attack server
-            if self.__attacker:
-                self.__attacker.prepare()
-
-            self.__sitl.start(prefix=sitl_prefix,
-                              speedup=speedup)
-
-            # connect to the vehicle
-            # dronekit is broken! it always tries to connect to 127.0.0.1:5760
-            print("try to connect to vehicle...")
-            print("connection timeout: %s" % self.__connection_timeout)
-            dronekit_connects_to = 'udp:127.0.0.1:14550'
-            vehicle = dronekit.connect(dronekit_connects_to,
+            # NOTE dronekit is broken!
+            #      it always tries to connect to 127.0.0.1:5760
+            logging.debug("trying to connect to vehicle [%s]", url_sitl)
+            vehicle = dronekit.connect(url_sitl,
                                        wait_ready=False,
-                                       heartbeat_timeout=self.__connection_timeout)
-            vehicle.wait_ready(True, timeout=self.__connection_timeout)
-            print("connected")
+                                       heartbeat_timeout=timeout_connection)
+            logging.debug("established connection with vehicle.")
+            logging.debug("waiting for vehicle to be ready.")
+            vehicle.wait_ready(True, timeout=timeout_connection)
+            logging.debug("vehicle is ready for mission.")
 
-            # launch the attack, if enabled
-            if self.__attacker:
-                self.__attacker.start()
+            # launch the attack, if one was provided
+            if attacker:
+                logging.debug("launching attack on vehicle")
+                attacker.start()
+                logging.debug("launched attack on vehicle")
+            else:
+                logging.debug("skipping attack launch: no attack provided.")
 
-            print("Using mission execution time limit: {} seconds".format(time_limit))
-            return self.__mission.execute(time_limit=time_limit,
-                                          vehicle=vehicle,
-                                          speedup=speedup,
-                                          timeout_heartbeat=liveness_timeout,
-                                          enable_workaround=enable_workaround,
-                                          check_wps=check_wps)
+            # execute the mission
+            return mission.execute(time_limit=timeout_mission,
+                                   vehicle=vehicle,
+                                   speedup=speedup,
+                                   timeout_heartbeat=timeout_liveness,
+                                   enable_workaround=enable_workaround,
+                                   check_wps=check_wps)
 
-        except mission.TimeoutError:
-            return (False, "timeout occurred")
-        finally:
-            if self.__attacker:
-                self.__attacker.stop()
-            if vehicle:
-                vehicle.close()
-            self.__sitl.stop()
+    # FIXME nicer exception -- different kinds?
+    except TimeoutException:
+        return (False, "timeout occurred")
+    finally:
+        if attacker:
+            logging.debug("closing attack server")
+            attacker.stop()
+            logging.debug("closed attack server")
+        if vehicle:
+            logging.debug("closing connection to vehicle")
+            vehicle.close()
+            logging.debug("closed connection to vehicle")
